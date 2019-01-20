3ba564e76907e9a085b13a9e68c9195e445bb6ef
Matheos Mattsson <matheos.mattsson@gmail.com>
2019-01-20 15:38:10 +0200

    cleaned up code and removed unnecessary features as well as made the output clearer

--- NetflixRemoteServer.py
+++ NetflixRemoteServer.py
@@ -2,7 +2,7 @@ import win32com.client as comclt, socket, threading,os, traceback
 
 wsh = comclt.Dispatch("WScript.shell")
 
-#Port for the server
+#Port for the server.
 port = 50096
 
 #Setting up ipv4 TCP Socket
@@ -10,7 +10,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("", port))
 s.listen(1)
-
+print "Server Started on port " + str(port)
 
 #Shuts down the whole server
 def shutDown():  
@@ -23,13 +23,11 @@ def shutDown():
 #Emulates keypresses
 def doAction(action):
     print(action)
-    wsh.AppActivate("Netflix") #Look for Window with name "Netflix"
+    wsh.AppActivate("Netflix -") #Look for Window with name "Netflix"
     if action == "actionPause":
        wsh.SendKeys("{ENTER}")
     elif action == "actionFullscreen":
         wsh.SendKeys("{F}")
-    elif action == "actionNoFullScreen":
-        wsh.SendKeys("{Esc}")
     elif action == "actionForward":
         wsh.SendKeys("{RIGHT}")
     elif action == "actionBack":
@@ -52,8 +50,6 @@ def server(connect, adress):
             elif data.lower()=="test": #Test statement one can send to server. If ok is received, is working
                 answer = "ok\n"
                 connect.send(answer.encode())
-            elif data.lower()=="exit": #'exit' command forces a madeup exception which leads to the except block being run which closes the connection (Stupid but works)
-                raise Exception("Haha")
             elif data.lower()=="shutdown":
                 shutDown()
             elif len(data)>0:
@@ -69,12 +65,13 @@ def server(connect, adress):
 #Infinite Loop for running the server using Threads
 while True:
     try:
-        
         connect, adress = s.accept()
-        print(adress[0]+" connected")
+        print(adress[0]+":")
         thread = threading.Thread(target=server, args=(connect, adress))
         thread.daemon = True
         thread.start()
+		
+		
         
     except:
         print("Server Crashed. Restarting...")
